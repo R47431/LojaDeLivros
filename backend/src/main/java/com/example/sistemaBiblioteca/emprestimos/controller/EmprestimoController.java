45ff2d1764880585b3fbf076b9863f03b8fb17e5
R47431 <joaorafael1223@hotmail.com>
2024-01-06 03:58:45 -0300

    logger

--- backend/src/main/java/com/example/sistemaBiblioteca/emprestimos/controller/EmprestimoController.java
+++ backend/src/main/java/com/example/sistemaBiblioteca/emprestimos/controller/EmprestimoController.java
@@ -2,7 +2,10 @@ package com.example.sistemaBiblioteca.emprestimos.controller;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +27,8 @@ import com.example.sistemaBiblioteca.livros.repository.LivroRepository;
 @RequestMapping("/emprestimo")
 public class EmprestimoController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmprestimoController.class);
+
     private final EmprestimosRepository emprestimosRepository;
     private final LivroRepository livroRepository;
     private final ClienteRepository clienteRepository;
@@ -41,31 +46,57 @@ public class EmprestimoController {
 
     @GetMapping("/{clienteId}")
     public ResponseEntity<ClienteComEmprestimosDto> getClienteComEmprestimos(@PathVariable Long clienteId) {
-        return clienteRepository.findById(clienteId)
-                .map(clienteComEmprestimosDto -> {
-                    List<EmprestimoComLivroDto> emprestimosDoCliente = emprestimoService
-                            .convertToDtoList(clienteComEmprestimosDto.getEmprestimos());
-                    ClienteComEmprestimosDto client = emprestimoService.toDto(clienteComEmprestimosDto);
-                    client.setEmprestimos(emprestimosDoCliente);
-                    return ResponseEntity.ok(client);
-                })
-                .orElse(ResponseEntity.notFound().build());
+        try {
+            Optional<ClienteModelo> clienteOptional = clienteRepository.findById(clienteId);
+
+            if (clienteOptional.isPresent()) {
+                ClienteModelo clienteComEmprestimosDto = clienteOptional.get();
+                List<EmprestimoComLivroDto> emprestimosDoCliente = emprestimoService
+                        .convertToDtoList(clienteComEmprestimosDto.getEmprestimos());
+                ClienteComEmprestimosDto client = emprestimoService.toDto(clienteComEmprestimosDto);
+                client.setEmprestimos(emprestimosDoCliente);
+                LOGGER.info("Cliente encontrado");
+                return ResponseEntity.ok(client);
+            } else {
+                LOGGER.warn("Nenhum cliente encontrado com ID: {}", clienteId);
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            LOGGER.error("Erro ao obter empréstimos do cliente com ID: {}", clienteId, e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @PostMapping
     public ResponseEntity<String> realizarEmprestimo(Long clienteId, LivroDto livroDto) {
-        return clienteRepository.findById(clienteId)
-                .map(cliente -> livroRepository.findById(livroDto.getLivroId())
-                        .map(livro -> {
-                            EmprestimoModelo emprestimo = new EmprestimoModelo();
-                            emprestimo.setCliente(cliente);
-                            emprestimo.setLivro(livro);
-                            emprestimo.setDataEmprestimo(LocalDate.now());
-                            emprestimosRepository.save(emprestimo);
-                            return ResponseEntity.status(HttpStatus.CREATED).body("Empréstimo realizado com sucesso");
-                        })
-                        .orElse(ResponseEntity.notFound().build()))
-                .orElse(ResponseEntity.notFound().build());
+        try {
+            Optional<ClienteModelo> clienteOptional = clienteRepository.findById(clienteId);
+            if (!clienteOptional.isPresent()) {
+                LOGGER.warn("Cliente não encontrado com ID: {}", clienteId);
+                return ResponseEntity.notFound().build();
+            }
+
+            ClienteModelo cliente = clienteOptional.get();
+            Optional<LivroModelo> livroOptional = livroRepository.findById(livroDto.getLivroId());
+            if (!livroOptional.isPresent()) {
+                LOGGER.warn("Livro não encontrado com ID: {}", livroDto.getLivroId());
+                return ResponseEntity.notFound().build();
+            }
+
+            LivroModelo livro = livroOptional.get();
+            EmprestimoModelo emprestimo = new EmprestimoModelo();
+            emprestimo.setCliente(cliente);
+            emprestimo.setLivro(livro);
+            emprestimo.setDataEmprestimo(LocalDate.now());
+            emprestimosRepository.save(emprestimo);
+            LOGGER.info("Empréstimo realizado com sucesso. Cliente ID: {}, Livro ID: {}", clienteId,
+                    livroDto.getLivroId());
+            return ResponseEntity.status(HttpStatus.CREATED).body("Empréstimo realizado com sucesso");
+        } catch (Exception e) {
+            LOGGER.error("Erro ao realizar empréstimo. Cliente ID: {}, Livro ID: {}", clienteId, livroDto.getLivroId(),
+                    e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @PostMapping("/{clienteId}/devolver/{livroId}/{emprestimoId}")
@@ -73,22 +104,35 @@ public class EmprestimoController {
             @PathVariable Long clienteId,
             @PathVariable Long livroId,
             @PathVariable Long emprestimoId) {
-        ClienteModelo cliente = getClienteOrThrow(clienteId);
-        LivroModelo livro = getLivroOrThrow(livroId);
-        EmprestimoModelo emprestimo = getEmprestimoOrThrow(emprestimoId);
+        try {
+            ClienteModelo cliente = getClienteOrThrow(clienteId);
+            LivroModelo livro = getLivroOrThrow(livroId);
+            EmprestimoModelo emprestimo = getEmprestimoOrThrow(emprestimoId);
 
-        if (emprestimo.getDataDevolucao() != null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Livro já foi devolvido anteriormente.");
+            if (emprestimo.getDataDevolucao() != null) {
+                LOGGER.warn("Tentativa de devolução de um empréstimo já devolvido. Empréstimo ID: {}", emprestimoId);
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Livro já foi devolvido anteriormente.");
+            }
+
+            if (!emprestimo.getCliente().equals(cliente) || !emprestimo.getLivro().equals(livro)) {
+                LOGGER.warn(
+                        "Dados de empréstimo inválidos para devolução. Cliente ID: {}, Livro ID: {}, Empréstimo ID: {}",
+                        clienteId, livroId, emprestimoId);
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                        "Dados de empréstimo inválidos para devolução.");
+            }
+
+            emprestimo.setDataDevolucao(LocalDate.now());
+            emprestimosRepository.save(emprestimo);
+
+            LOGGER.info("Devolução realizada com sucesso. Empréstimo ID: {}", emprestimo.getEmprestimoId());
+            return ResponseEntity.ok("Devolução realizada com sucesso. Empréstimo ID: " + emprestimo.getEmprestimoId());
+        } catch (Exception e) {
+            LOGGER.error("Erro ao realizar devolução. Cliente ID: {}, Livro ID: {}, Empréstimo ID: {}", clienteId,
+                    livroId, emprestimoId, e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+
         }
-
-        if (!emprestimo.getCliente().equals(cliente) || !emprestimo.getLivro().equals(livro)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Dados de empréstimo inválidos para devolução.");
-        }
-
-        emprestimo.setDataDevolucao(LocalDate.now());
-        emprestimosRepository.save(emprestimo);
-
-        return ResponseEntity.ok("Devolução realizada com sucesso. Empréstimo ID: " + emprestimo.getEmprestimoId());
     }
 
     // TODO muda para um service
